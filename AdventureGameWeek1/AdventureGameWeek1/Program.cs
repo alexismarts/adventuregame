f1f83802d8c6e9338aa998b60aa192cbcc3b1a29
alexismarts <alexis.marts@loop.colum.edu>
2017-02-11 10:15:52 -0600

    Add files via upload

---
+++ AdventureGameWeek1/AdventureGameWeek1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Adventure
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Saving the EPA, the Arts, Women's Rights, and the World from the President");
+            Console.WriteLine("This is a wonderful game where we try to rescue America from censorship.");
+            Console.Read();
+        }
+    }
+}
